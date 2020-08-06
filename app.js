c096b98393daa60536f5f46adae9462937b0f0b1
Mirlind Murati <murati@ut.ee>
2020-08-06 17:50:56 +0300

    Command line argument provided.

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const request = require('request')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-// Calling the geocode function defined in the Utils/geocode  
-geocode('Tartu', (error, data) => {
-    if(error){
-      return console.log(error)
-    }
+
+const address = process.argv[2]
+
+if(!address) {
+    console.log('Please provide an address')
+} else {
+
+    // Calling the geocode function defined in the Utils/geocode  
+    geocode(address, (error, data) => {
+        if(error){
+        return console.log(error)
+        }
 
     // Calling the forecast function defined in the Utils/geocode  
     forecast(data.latitude, data.longitude, (error, forecastData) => {
@@ -16,6 +23,6 @@ geocode('Tartu', (error, data) => {
         console.log(data.location)
         console.log(forecastData)
       
+        })
     })
-})
-
+}
