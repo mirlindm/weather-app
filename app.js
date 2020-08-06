239e951fb8fc680d0f228a86e05aab7d62bcb711
Mirlind Murati <murati@ut.ee>
2020-08-06 14:05:35 +0300

    Changes made to get the response in JSON format.

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=585fe9c36b0563d8091a7e0840ef32db&query=37.8267,-122.4233'
 
 // Function to make the HTPP Request
-request({url: url}, (error, response) => {
-    console.log(response)
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current)
+    console.log('It is currently ' + response.body.current.temperature + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
 })
